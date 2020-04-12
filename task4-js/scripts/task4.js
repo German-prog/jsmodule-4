e90fee8c73cca80bc4ca76a9532509cb453bedf6
German-prog <56883343+German-prog@users.noreply.github.com>
2020-04-12 11:17:50 +0300

    Add files via upload

---
+++ task4-js/scripts/task4.js
@@ -0,0 +1,7 @@
+
+ let nameH = prompt("Введіть ім’я улюбленого героя") 
+if (nameH.length < 6) {
+alert(nameH)
+} else {
+    alert("кількість літер більше 6")
+}
---
+++ task5-js/scripts/task5.js
@@ -0,0 +1,7 @@
+const array = [5, 3, 6, 2, 4];
+let result = 0;
+for (let i = 0; i < array.length; i++) {
+  result += array [i];
+  console.log(array[i]);
+}
+console.log("result:",result);
